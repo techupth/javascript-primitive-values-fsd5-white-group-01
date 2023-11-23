eb755ff5d7987802ceb1ef021d2c0ed91b513c7a
NapatKan <147288328+NapatKan@users.noreply.github.com>
2023-11-23 14:09:22 +0700

    feat: submitting completed exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalBill = 27000;
+let quarterDiscount = (totalBill * 25) / 100;
+let discountedBill = totalBill - quarterDiscount;
+console.log(discountedBill);
