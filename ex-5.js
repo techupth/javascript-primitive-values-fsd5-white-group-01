204e65a3fa6fb47e9d1c834ce99e8d3baec0b075
Pacharadanai <pacharadanaipinake@gmail.com>
2023-11-23 13:58:11 +0700

    feat: add completed exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let price = 27000;
+let discount = 0.25 * price;
+let netPrice = price - discount;
+console.log(discount);
+console.log(netPrice);
